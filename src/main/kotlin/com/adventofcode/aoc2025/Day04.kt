eb7a2e3c42e836b101444d7f2ea9cb546557a556
Justin Eberlein <jmeberlein@gmail.com>
2025-12-04 13:23:02 -0600

    Translate day 4

---
+++ src/main/kotlin/com/adventofcode/aoc2025/Day04.kt
@@ -0,0 +1,90 @@
+package com.adventofcode.aoc2025
+
+import org.openjdk.jmh.annotations.*
+import com.adventofcode.util.*
+import java.util.concurrent.TimeUnit
+import org.apache.commons.math3.complex.Complex
+
+@State(Scope.Thread)
+open class State04(fileName: String = "src/main/resources/input/2025/04.txt") : File(fileName) {
+    var grid: Grid<Boolean>
+
+    init {
+        this.grid = Grid<Boolean>(this.size, this[0].length)
+        for (r in 0..<this.size) {
+            for (c in 0..<this[0].length) {
+                grid[r, c] = this[r][c] == '@'
+            }
+        }
+    }
+}
+
+open class Day04 {
+    fun part1(state: File): Long {
+        return part1(state as State04)
+    }
+
+    fun part2(state: File): Long {
+        return part2(state as State04)
+    }
+
+    @Benchmark
+    @BenchmarkMode(Mode.SampleTime)
+    @OutputTimeUnit(TimeUnit.MILLISECONDS)
+    fun part1(state: State04): Long {
+        var res = 0L
+        for (r in 0..<state.grid.rows) {
+            for (c in 0..<state.grid.cols) {
+                val p = Complex(r.toDouble(), c.toDouble())
+                if (state.grid[p] && countAdjacencies(state.grid, p) < 4) {
+                    res++
+                }
+            }
+        }
+        return res
+    }
+
+    @Benchmark
+    @BenchmarkMode(Mode.SampleTime)
+    @OutputTimeUnit(TimeUnit.MILLISECONDS)
+    fun part2(state: State04): Long {
+        var res = 0L
+        var curr = 0L
+        do {
+            curr = 0L
+            var toRemove = ArrayList<Complex>()
+            for (r in 0..<state.grid.rows) {
+                for (c in 0..<state.grid.cols) {
+                    val p = Complex(r.toDouble(), c.toDouble())
+                    if (state.grid[p] && countAdjacencies(state.grid, p) < 4) {
+                        toRemove.add(p)
+                        curr++
+                    }
+                }
+            }
+
+            for (p in toRemove) {
+                state.grid[p] = false
+            }
+
+            res += curr
+        } while (curr != 0L)
+        return res
+    }
+
+    fun countAdjacencies(grid: Grid<Boolean>, p: Complex): Int {
+        val ADJACENCIES: Array<Complex> = arrayOf(
+            Complex(-1.0, -1.0), Complex(-1.0,  0.0), Complex(-1.0,  1.0),
+            Complex( 0.0, -1.0),                      Complex( 0.0,  1.0),
+            Complex( 1.0, -1.0), Complex( 1.0,  0.0), Complex( 1.0,  1.0)
+        )
+
+        var adj = 0
+        for (offset: Complex in ADJACENCIES) {
+            if (grid.getOrDefault(offset.add(p), false)) {
+                adj++
+            }
+        }
+        return adj
+    }
+}
--- src/main/kotlin/com/adventofcode/util/Grid.kt
+++ src/main/kotlin/com/adventofcode/util/Grid.kt
@@ -2,7 +2,7 @@ package com.adventofcode.util
 
 import org.apache.commons.math3.complex.Complex
 
-class Grid<T>(rows: Int, cols: Int) {
+class Grid<T>(val rows: Int, val cols: Int) {
     val grid = Array(rows) { arrayOfNulls<Any?>(cols) }
 
     operator fun <T> get(i: Complex): T {
@@ -16,6 +16,23 @@ class Grid<T>(rows: Int, cols: Int) {
     fun <T> getOrDefault(i: Complex, default: T): T {
         try {
             val tmp = grid[i.getReal().toInt()][i.getImaginary().toInt()] as T?
+            return tmp ?: default
+        } catch (e: IndexOutOfBoundsException) {
+            return default
+        }
+    }
+
+    operator fun <T> get(r: Int, c: Int): T {
+        return grid[r][c] as T
+    }
+
+    operator fun <T> set(r: Int, c: Int, v: T) {
+        grid[r][c] = v
+    }
+    
+    fun <T> getOrDefault(r: Int, c: Int, default: T): T {
+        try {
+            val tmp = grid[r][c] as T?
             return tmp ?: default
         } catch (e: IndexOutOfBoundsException) {
             return default
